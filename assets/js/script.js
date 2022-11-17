403e0ff3e6414819fae357735e6b9ed43d3fc8a0
EgorovZaxar555 <101442263+EgorovZaxar555@users.noreply.github.com>
2022-11-17 23:12:11 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+const animItems = document.querySelectorAll('._anime-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_animal-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+
+}
+
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+
+for (let el of tabLinks) {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+
+        document.querySelector(".tabs li.active").classList.remove("active");
+        document.querySelector(".tabs-panel.active").classList.remove("active");
+
+        const parentListItem = el.parentElement;
+        parentListItem.classList.add("active");
+        const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+        const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+        panel[0].classList.add("active");
+    });
+}
+
+
+
+//   Видео в модальном окне
+$('a.lightbox').divbox({ caption: false });
+$('a.lightbox2').divbox({ width: 600, height: 400, caption: false });
+
+
+
+
+
+
